62e54ca11ec4083007911a5f4b6162dc0c96a7d7
Eric Ricielle <suporte@tucanoweb.com.br>
2024-09-24 08:16:21 -0300

    Update servico-pagamentos.module.ts

--- src/servico-pagamentos/servico-pagamentos.module.ts
+++ src/servico-pagamentos/servico-pagamentos.module.ts
@@ -8,11 +8,11 @@ import { Pagamento } from '../entities';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '95.216.252.56',
+      host: 'XXXXXXXXX',
       port: 3306,
-      username: 'gabriela-payments',
-      password: 'Fyt6PK4kFmxkCMdK',
-      database: 'gabriela-payments',
+      username: 'XXXXXXXXX',
+      password: 'XXXXXXXXX',
+      database: 'XXXXXXXXX',
       entities: [Pagamento],
       synchronize: true,
       name: 'paymentsConnection', // Nome único para a conexão de pagamentos - necessário pois está sendo criada uma segunda conexão, diferente da conexão criada em global (app.module.ts)
